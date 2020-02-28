a58c01f7cd398fabfd18fa810253ac3b6a98efcb
Atar Sidhu <a.sidhu14@hotmail.com>
2020-02-27 22:33:36 -0800

    Created two image buttons and linked them to their respective pages

--- app/src/main/java/com/example/cardgames/MainActivity.java
+++ app/src/main/java/com/example/cardgames/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.cardgames;
 
+import android.content.Intent;
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton goFishImageBtn = findViewById(R.id.imageButtonGoFish);
+        goFishImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), GoFish.class);
+                startActivity(startIntent);
+            }
+        });
+
+        ImageButton blackjackImageBtn = findViewById(R.id.imageButtonBlackjack);
+        blackjackImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Blackjack.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
