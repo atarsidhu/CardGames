1ff64993cff5d4079c239519eea8a53f498a17a9
theolaa <theolaa@hotmail.ca>
2020-03-08 14:59:15 -0700

    New Art + Start of Card System

--- app/src/main/java/com/example/cardgames/MainActivity.java
+++ app/src/main/java/com/example/cardgames/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
