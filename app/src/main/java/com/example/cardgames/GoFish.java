b487d0b836accb8354f38e10906ac632ff853bd4
theolaa <theolaa@hotmail.ca>
2020-03-11 23:44:10 -0700

    Card System Work

--- app/src/main/java/com/example/cardgames/GoFish.java
+++ app/src/main/java/com/example/cardgames/GoFish.java
@@ -1,13 +1,20 @@
 package com.example.cardgames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class GoFish extends AppCompatActivity {
+
+    ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_fish);
+        mainLayout = findViewById(R.id.go_fish_main_layout);
     }
 }
