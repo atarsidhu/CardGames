aac5839ec9a13a2176dd70515bb0634f3cfb643f
Atar Sidhu <a.sidhu14@hotmail.com>
2020-04-07 17:03:14 -0700

    Fixed timing issue when "Match from Go Fish card" was not displayed. Added tie game message

--- app/src/main/java/com/example/cardgames/GoFish.java
+++ app/src/main/java/com/example/cardgames/GoFish.java
@@ -44,6 +44,7 @@ public class GoFish extends AppCompatActivity {
     private boolean currentlyPlayerTurn;
     private boolean lockPlayer = true;
     private boolean easyDifficulty = true;
+    private boolean matchFromGoFishCard = false;
 
     // Constants
     private int STARTING_HAND_SIZE = 7;
@@ -62,6 +63,8 @@ public class GoFish extends AppCompatActivity {
             lockPlayer = true;
             rankSelector.setVisibility(View.GONE);
             String gameOverMessage = (AI.getScore() > human.getScore()) ? "Game Over!\nYou Lose.\nPlay Again?" : "Game Over!\nYou Won!\nPlay Again?";
+            if(AI.getScore() == human.getScore())
+                gameOverMessage = "Game Over!\nIt's a Tie!\nPlay Again?";
             centreText.setText(gameOverMessage);
             centreText.setVisibility(View.VISIBLE);
             Log.i("Go Fish", "Game Over!");
@@ -175,6 +178,7 @@ public class GoFish extends AppCompatActivity {
                             public void run() {
                                 centreText.setText(R.string.ai_match_found_from_draw);
                                 centreText.setVisibility(View.VISIBLE);
+                                matchFromGoFishCard = true;
                             }
                         }, 7 * POPUP_DISPLAY_DURATION);
                     }
@@ -187,6 +191,7 @@ public class GoFish extends AppCompatActivity {
                             public void run() {
                                 centreText.setText(R.string.match_found_from_draw);
                                 centreText.setVisibility(View.VISIBLE);
+                                matchFromGoFishCard = true;
                             }
                         }, 4*POPUP_DISPLAY_DURATION);
                     }
@@ -236,18 +241,35 @@ public class GoFish extends AppCompatActivity {
                 }
             }, 5*POPUP_DISPLAY_DURATION);
 
-            // Hide Centre Text and play a new round
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    centreText.setVisibility(View.GONE);
-                    try {
-                        playRound();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            //If match found or if !match from go fish card, speed up the play round
+            if(!returnedCards.isEmpty() || !matchFromGoFishCard){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        centreText.setVisibility(View.GONE);
+                        try {
+                            playRound();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-            }, 9*POPUP_DISPLAY_DURATION);
+                }, 7*POPUP_DISPLAY_DURATION);
+            }else{
+                // Hide Centre Text and play a new round
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        centreText.setVisibility(View.GONE);
+                        try {
+                            playRound();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, 9*POPUP_DISPLAY_DURATION);
+            }
+
+
         }
 
         // Player's Turn
@@ -273,18 +295,32 @@ public class GoFish extends AppCompatActivity {
                 }
             }, 2*POPUP_DISPLAY_DURATION);
 
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    centreText.setVisibility(View.GONE);
-                    try {
-                        playRound();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            //If match found or if !match from go fish card, speed up the play round
+            if(!returnedCards.isEmpty() || !matchFromGoFishCard){
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        centreText.setVisibility(View.GONE);
+                        try {
+                            playRound();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-            },6*POPUP_DISPLAY_DURATION);
+                }, 4*POPUP_DISPLAY_DURATION);
+            }else{
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        centreText.setVisibility(View.GONE);
+                        try {
+                            playRound();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },6*POPUP_DISPLAY_DURATION);
+            }
         }
     }
 
@@ -400,6 +436,7 @@ public class GoFish extends AppCompatActivity {
             rankSelector.setVisibility(View.GONE);
         }
         centreText.setVisibility(View.VISIBLE);
+        matchFromGoFishCard = false;
     }
 
     private void setupUI() {
@@ -479,8 +516,8 @@ public class GoFish extends AppCompatActivity {
         }
 
         // Scores
-        humanScore.setText(String.valueOf(human.getScore()));
-        aiScore.setText(String.valueOf(AI.getScore()));
+        humanScore.setText(String.format(getResources().getString(R.string.your_score), human.getScore()));
+        aiScore.setText(String.format(getResources().getString(R.string.ai_score), AI.getScore()));
 
         // Cards left in Deck
         cardsLeftInDeck.setText(String.format(getResources().getString(R.string.cards_left_in_deck), deck.size()));
